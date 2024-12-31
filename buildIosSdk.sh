87af5a7a8480752c3c0bb3feab6f71ccf9be5e02
mhsnprvr <mhsnprvr@gmail.com>
2025-01-01 02:36:53 +0530

    same

---
+++ buildIosSdk.sh
@@ -0,0 +1,31 @@
+# Clean output directory
+rm -rf ios/sdk/out
+mkdir -p ios/sdk/out
+
+
+mkdir -p ios/sdk/out
+xcodebuild clean \
+    -workspace ios/jitsi-meet.xcworkspace \
+    -scheme JitsiMeetSDK
+xcodebuild archive \
+    -workspace ios/jitsi-meet.xcworkspace \
+    -scheme JitsiMeetSDK  \
+    -configuration Release \
+    -sdk iphonesimulator \
+    -destination='generic/platform=iOS Simulator' \
+    -archivePath ios/sdk/out/ios-simulator \
+    SKIP_INSTALL=NO \
+    BUILD_LIBRARY_FOR_DISTRIBUTION=YES
+xcodebuild archive \
+    -workspace ios/jitsi-meet.xcworkspace \
+    -scheme JitsiMeetSDK  \
+    -configuration Release \
+    -sdk iphoneos \
+    -destination='generic/platform=iOS' \
+    -archivePath ios/sdk/out/ios-device \
+    SKIP_INSTALL=NO \
+    BUILD_LIBRARY_FOR_DISTRIBUTION=YES
+xcodebuild -create-xcframework \
+    -framework ios/sdk/out/ios-device.xcarchive/Products/Library/Frameworks/JitsiMeetSDK.framework \
+    -framework ios/sdk/out/ios-simulator.xcarchive/Products/Library/Frameworks/JitsiMeetSDK.framework \
+    -output ios/sdk/out/JitsiMeetSDK.xcframework
