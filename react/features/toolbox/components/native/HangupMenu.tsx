aa83d00662a0d7333107d9a74f240cff2e1133cf
Mohsen <mhsnprvr@gmail.com>
2025-09-17 16:19:37 +0330

    renew_start

--- react/features/toolbox/components/native/HangupMenu.tsx
+++ react/features/toolbox/components/native/HangupMenu.tsx
@@ -1,21 +1,21 @@
-import React, { useCallback } from 'react';
-import { View } from 'react-native';
-import { useDispatch, useSelector } from 'react-redux';
+import React, { useCallback } from "react";
+import { View } from "react-native";
+import { useDispatch, useSelector } from "react-redux";
 
-import { createBreakoutRoomsEvent, createToolbarEvent } from '../../../analytics/AnalyticsEvents';
-import { sendAnalytics } from '../../../analytics/functions';
-import { appNavigate } from '../../../app/actions';
-import { IReduxState } from '../../../app/types';
-import ColorSchemeRegistry from '../../../base/color-scheme/ColorSchemeRegistry';
-import { endConference } from '../../../base/conference/actions';
-import { hideSheet } from '../../../base/dialog/actions';
-import BottomSheet from '../../../base/dialog/components/native/BottomSheet';
-import { PARTICIPANT_ROLE } from '../../../base/participants/constants';
-import { getLocalParticipant } from '../../../base/participants/functions';
-import Button from '../../../base/ui/components/native/Button';
-import { BUTTON_TYPES } from '../../../base/ui/constants.native';
-import { moveToRoom } from '../../../breakout-rooms/actions';
-import { isInBreakoutRoom } from '../../../breakout-rooms/functions';
+import { createBreakoutRoomsEvent, createToolbarEvent } from "../../../analytics/AnalyticsEvents";
+import { sendAnalytics } from "../../../analytics/functions";
+import { appNavigate } from "../../../app/actions";
+import { IReduxState } from "../../../app/types";
+import ColorSchemeRegistry from "../../../base/color-scheme/ColorSchemeRegistry";
+import { endConference } from "../../../base/conference/actions";
+import { hideSheet } from "../../../base/dialog/actions";
+import BottomSheet from "../../../base/dialog/components/native/BottomSheet";
+import { PARTICIPANT_ROLE } from "../../../base/participants/constants";
+import { getLocalParticipant } from "../../../base/participants/functions";
+import Button from "../../../base/ui/components/native/Button";
+import { BUTTON_TYPES } from "../../../base/ui/constants.native";
+import { moveToRoom } from "../../../breakout-rooms/actions";
+import { isInBreakoutRoom } from "../../../breakout-rooms/functions";
 
 /**
  * Menu presenting options to leave a room or meeting and to end meeting.
@@ -24,51 +24,56 @@ import { isInBreakoutRoom } from '../../../breakout-rooms/functions';
  */
 function HangupMenu() {
     const dispatch = useDispatch();
-    const _styles: any = useSelector((state: IReduxState) => ColorSchemeRegistry.get(state, 'Toolbox'));
+    const _styles: any = useSelector((state: IReduxState) => ColorSchemeRegistry.get(state, "Toolbox"));
     const inBreakoutRoom = useSelector(isInBreakoutRoom);
-    const isModerator = useSelector((state: IReduxState) =>
-        getLocalParticipant(state)?.role === PARTICIPANT_ROLE.MODERATOR);
+    const isModerator = useSelector(
+        (state: IReduxState) => getLocalParticipant(state)?.role === PARTICIPANT_ROLE.MODERATOR
+    );
     const { DESTRUCTIVE, SECONDARY } = BUTTON_TYPES;
 
     const handleEndConference = useCallback(() => {
         dispatch(hideSheet());
-        sendAnalytics(createToolbarEvent('endmeeting'));
+        sendAnalytics(createToolbarEvent("endmeeting"));
         dispatch(endConference());
-    }, [ hideSheet ]);
+    }, [hideSheet]);
 
     const handleLeaveConference = useCallback(() => {
         dispatch(hideSheet());
-        sendAnalytics(createToolbarEvent('hangup'));
+        sendAnalytics(createToolbarEvent("hangup"));
         dispatch(appNavigate(undefined));
-    }, [ hideSheet ]);
+    }, [hideSheet]);
 
     const handleLeaveBreakoutRoom = useCallback(() => {
         dispatch(hideSheet());
-        sendAnalytics(createBreakoutRoomsEvent('leave'));
+        sendAnalytics(createBreakoutRoomsEvent("leave"));
         dispatch(moveToRoom());
-    }, [ hideSheet ]);
+    }, [hideSheet]);
 
     return (
         <BottomSheet>
-            <View style = { _styles.hangupMenuContainer }>
-                { isModerator && <Button
+            <View style={_styles.hangupMenuContainer}>
+                {/* { isModerator && <Button
                     accessibilityLabel = 'toolbar.endConference'
                     labelKey = 'toolbar.endConference'
                     onClick = { handleEndConference }
                     style = { _styles.hangupButton }
-                    type = { DESTRUCTIVE } /> }
+                    type = { DESTRUCTIVE } /> } */}
                 <Button
-                    accessibilityLabel = 'toolbar.leaveConference'
-                    labelKey = 'toolbar.leaveConference'
-                    onClick = { handleLeaveConference }
-                    style = { _styles.hangupButton }
-                    type = { SECONDARY } />
-                { inBreakoutRoom && <Button
-                    accessibilityLabel = 'breakoutRooms.actions.leaveBreakoutRoom'
-                    labelKey = 'breakoutRooms.actions.leaveBreakoutRoom'
-                    onClick = { handleLeaveBreakoutRoom }
-                    style = { _styles.hangupButton }
-                    type = { SECONDARY } /> }
+                    accessibilityLabel="toolbar.leaveConference"
+                    labelKey="toolbar.leaveConference"
+                    onClick={handleLeaveConference}
+                    style={_styles.hangupButton}
+                    type={DESTRUCTIVE}
+                />
+                {inBreakoutRoom && (
+                    <Button
+                        accessibilityLabel="breakoutRooms.actions.leaveBreakoutRoom"
+                        labelKey="breakoutRooms.actions.leaveBreakoutRoom"
+                        onClick={handleLeaveBreakoutRoom}
+                        style={_styles.hangupButton}
+                        type={SECONDARY}
+                    />
+                )}
             </View>
         </BottomSheet>
     );
